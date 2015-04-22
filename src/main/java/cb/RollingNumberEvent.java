d56864e7c863ba5174611f3902a3ffe1108a9aec
gyeh <gyeh@paypal.com>
2015-04-21 23:00:47 -0700

    import netflix rolling window impl

---
+++ src/main/java/cb/RollingNumberEvent.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright 2012 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cb;
+
+/**
+ * Various states/events that can be captured in the {@link RollingNumber}.
+ * <p>
+ * Note that events are defined as different types:
+ * <ul>
+ * <li>Counter: <code>isCounter() == true</code></li>
+ * <li>MaxUpdater: <code>isMaxUpdater() == true</code></li>
+ * </ul>
+ * <p>
+ * The Counter type events can be used with {@link RollingNumber#increment}, {@link RollingNumber#add}, {@link RollingNumber#getRollingSum} and others.
+ * <p>
+ * The MaxUpdater type events can be used with {@link RollingNumber#updateRollingMax} and {@link RollingNumber#getRollingMaxValue}.
+ */
+public enum RollingNumberEvent {
+    SUCCESS(1), FAILURE(1);
+
+    private final int type;
+
+    private RollingNumberEvent(int type) {
+        this.type = type;
+    }
+
+    public boolean isCounter() {
+        return type == 1;
+    }
+
+    public boolean isMaxUpdater() {
+        return type == 2;
+    }
+}
